526b8b40cb37254faac3469a809b5cceeaba9fdf
McKay Fenn <mckay@flitchcreative.com>
2023-11-15 14:57:09 -0700

    Adding to the contact forms

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,13 +4,15 @@ import { useMutation, gql } from '@apollo/client';
 
 // import { CREATE_CONTACT_MUTATION } from '../graphql/mutations';
 const CREATE_CONTACT_MUTATION = gql`
-  mutation CreateContact($email: String!, $fullname: String!, $message: String!) {
-    createContactForm(data: { Email: $email, Name: $fullname, Message: $message }) {
+  mutation CreateContact($email: String!, $fullname: String!, $phone: String!, $company: String!, $message: String!) {
+    createContactForm(data: { Email: $email, Name: $fullname, Phone: $phone, Company: $company, Message: $message }) {
       data {
         id 
         attributes {
             Name
             Email
+            Phone
+            Company
             Message
         }
       }
@@ -22,10 +24,10 @@ export default function ContactForm() {
   const [createEmailEntry] = useMutation(CREATE_CONTACT_MUTATION);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const { email, fullname, message } = event.target.elements;
+    const { email, fullname, phone, company, message } = event.target.elements;
 
     try {
-      await createEmailEntry({ variables: { email: email.value, fullname: fullname.value, message: message.value } });
+      await createEmailEntry({ variables: { email: email.value, fullname: fullname.value, phone: phone.value, company: company.value, message: message.value } });
       // Handle success - maybe clear the form or show a success message
       setFormSubmitted(true);
     } catch (error) {
@@ -34,7 +36,7 @@ export default function ContactForm() {
   };
 
   if (formSubmitted) {
-    return <div><h3>Thank you for your submission!</h3></div>;
+    return <div><h3 className='green'>Thank you for your submission!</h3></div>;
   }
 
   return (
@@ -55,6 +57,22 @@ export default function ContactForm() {
             id="email"
             name="email"
             type="email"
+            />
+        </div>
+        <div className='form-input'>
+            <label htmlFor="phone">Phone</label>
+            <input
+            id="phone"
+            name="phone"
+            type="tel"
+            />
+        </div>
+        <div className='form-input'>
+            <label htmlFor="company">Company</label>
+            <input
+            id="company"
+            name="company"
+            type="text"
             />
         </div>
         <div className='form-input'>
