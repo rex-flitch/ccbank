ce83ebecb830b1ba99dab66a684eca3b7f692073
McKay Fenn <mckay@flitchcreative.com>
2024-02-09 16:53:10 -0700

    Changes made throughout

--- src/pages/FinancialLibrary.js
+++ src/pages/FinancialLibrary.js
@@ -5,45 +5,59 @@ import parse from 'html-react-parser'
 import AccountLogin from '../components/AccountLogin'
 const FINANCIALLIBRARYQUERY = gql`
     query getfinancialLibrary {
-        financialLibrary {
-            data {
-              id
-              attributes {
-                Hero {
-                  Title
-                  Description
-                  ButtonTitle
-                  ButtonURL
-                  BackgroundImage {
-                    data {
-                      attributes {
-                        url
-                        alternativeText
-                      }
-                    }
+      financialLibrary {
+        data {
+          id
+          attributes {
+            Hero {
+              Title
+              Description
+              ButtonTitle
+              ButtonURL
+              BackgroundImage {
+                data {
+                  attributes {
+                    url
+                    alternativeText
                   }
                 }
-                Title
-                FinancialToolsButton
-                FinancialToolsButtonURL
-                ImageCTA {
-                  id
-                  Title
-                  Description
-                  ButtonURL
-                  ButtonTitle
-                  Image {
-                    data {
-                      attributes {
-                        url
-                        alternativeText
-                      }
-                    }
+              }
+            }
+            Title
+            FinancialToolsButton
+            FinancialToolsButtonURL
+            ImageCTA {
+              id
+              Title
+              Description
+              ButtonURL
+              ButtonTitle
+              Image {
+                data {
+                  attributes {
+                    url
+                    alternativeText
+                  }
+                }
+              }
+            }
+            FLImageCTA {
+              Title
+              Description
+              ButtonURL
+              ButtonTitle
+              Image {
+                data {
+                  attributes {
+                    url
+                    alternativeText
                   }
                 }
               }
             }
           }
+        }
+      }
     }
 `
 export default function FinancialLibrary() {
@@ -75,10 +89,19 @@ export default function FinancialLibrary() {
           </div>
       </div>
       <div className='cta-wrapper col-3 img-250'>
-        <h2 className='center orange'>{data.financialLibrary.data.attributes.Title}</h2>
-        <hr className='green center'></hr>
-        <div className='btn-green center mg-top-50'><Link to={data.financialLibrary.data.attributes.FinancialToolsButtonURL}>{data.financialLibrary.data.attributes.FinancialToolsButton}</Link></div>
-            <div className='cta-box container mg-top-50'>
+        {/* <h2 className='center orange'>{data.financialLibrary.data.attributes.Title}</h2>
+        <hr className='green center'></hr> */}
+        <div className='sbaloans-intro container mg-top-50 mg-bottom-50'>
+        <div className='sba-text'>
+            <h2>{data.financialLibrary.data.attributes.FLImageCTA.Title}</h2>
+            <hr className='green'></hr>
+            <div className='mg-top-20'>{parse(data.financialLibrary.data.attributes.FLImageCTA.Description)}</div>
+            <div className='btn-green center mg-top-50'><Link to={data.financialLibrary.data.attributes.FLImageCTA.ButtonURL}>{data.financialLibrary.data.attributes.FLImageCTA.ButtonTitle}</Link></div>
+        </div>
+        <div className='sba-image'><img src={data.financialLibrary.data.attributes.FLImageCTA.Image.data.attributes.url} alt={data.financialLibrary.data.attributes.FLImageCTA.Image.data.attributes.alternativeText} /></div>
+      </div>
+        {/* <div className='btn-green center mg-top-50'><Link to={data.financialLibrary.data.attributes.FinancialToolsButtonURL}>{data.financialLibrary.data.attributes.FinancialToolsButton}</Link></div> */}
+            <div className='cta-box container mg-top-100'>
                 {data.financialLibrary.data.attributes.ImageCTA.map((cta) => (
                     <div key={cta.id} className='cta'>
                         <div className='cta-image' style={{backgroundImage: `url(${cta.Image.data.attributes.url})`}}></div>
