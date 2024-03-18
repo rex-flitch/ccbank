4eca06dc0d255d5c273395214caacb834a724adb
McKay Fenn <mckay@flitchcreative.com>
2024-03-18 14:39:19 -0600

    Changing the financial tools page

--- src/pages/FinancialLibrary.js
+++ src/pages/FinancialLibrary.js
@@ -109,7 +109,9 @@ export default function FinancialLibrary() {
                             <div className='title'><h2>{cta.Title}</h2></div>
                             <hr className='green'></hr>
                             <div className='desciption'><p>{parse(cta.Description)}</p></div>
-                            <div className='btn-ghost-green'><Link reloadDocument to={cta.ButtonURL}>Read more</Link></div>
+                            {cta.ButtonTitle !== null &&
+                              <div className='btn-ghost-green'><Link reloadDocument to={cta.ButtonURL}>{cta.ButtonTitle}</Link></div>
+                            }
                         </div>
                     </div>
                 ))}
